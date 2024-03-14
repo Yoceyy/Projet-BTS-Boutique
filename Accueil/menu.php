fee5f4f1ee29398f46a9196d0c6ddc888a7db75e
Yo <123593814+Yoceyy@users.noreply.github.com>
2024-03-14 14:28:17 +0100

    Menu v1

--- Accueil/menu.php
+++ Accueil/menu.php
@@ -1,13 +1,23 @@
-<body>
-	<section class="nav">
-		<div class="name">
-			<p>GEEKZONE</p>		
-		</div>
-		<div class="info">
-			<li class="Acceuil"><a href="../Accueil/index.php">Acceuil</a></li>
-		<li><a href="../Produit/produit.php">Nos produits</a></li>
-		<li><a href="../Magasin/magasin.php">Nos Magasin</a></li>
-		</div>
-		
-	</section>
-</body>
+<h1>GEEKZONE</h1>
+<nav>
+  <ul>
+    <li class=""><a href="../Accueil/index.php">Accueil</a>
+    </li>
+    <li class="deroulant"><a href="../Produit/produit.php">Nos produit &ensp;</a>
+      <ul class="sous">
+        <li><a href="#">USB</a></li>
+        <li><a href="#">Frigo</a></li>
+        <li><a href="#">Masque</a></li>
+      </ul>
+    </li>
+	<li class="deroulant"><a href="../Magasin/magasin.php">Nos Magasin &ensp;</a>
+      <ul class="sous">
+        <li><a href="#">Marseille</a></li>
+        <li><a href="#">Paris</a></li>
+        <li><a href="#">Nice</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">A propos</a></li>
+  </ul>
+</nav>
